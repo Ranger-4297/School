4b0fa3f4b6cd77ebd042eaf98270983a49da03e3
Ranger-4297 <a.rhykerw@gmail.com>
2021-09-22 11:41:23 +0100

    Folder add & file name

---
+++ GCSE-CS/Revision.py
@@ -0,0 +1,16 @@
+# Start import
+import tkinter
+from tkinter import *
+from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import messagebox
+from tkinter import filedialog
+# End import
+
+# GUI
+root = Tk()
+root.title("Revision") # Name of window
+root.geometry("500x300") # Size of window
+root.resizable(0,0) # Prevent window resizing
+root.config(bg="#2f3136") # Window background
+
+# load GUI
